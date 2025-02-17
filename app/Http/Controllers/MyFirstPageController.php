1cf8be05271344ab606c7a3162042879b283e737
Ilya_Evdokimov <blinix03yt@gmail.com>
2025-02-17 14:53:50 +0500

    new

--- app/Http/Controllers/MyFirstPageController.php
+++ app/Http/Controllers/MyFirstPageController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class MyFirstPageController extends Controller
 {
     public function index() {
-        return 'This is my page!';
+        return 'This is my page';
     }
 }
